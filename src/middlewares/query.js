be059feef7b2c1e5e29fe05298011dcd7a06f3ba
Michele Triaca <michele.triaca@telnext.com>
2019-11-24 18:57:02 +0100

    [refactoring] minor refactoring

--- src/middlewares/query.js
+++ src/middlewares/query.js
@@ -14,22 +14,22 @@ const pool = new Pool({
 const QuerySyncMiddleware = async (req, res, next) => {
   let client
   try {
-    const { query, postProcessFn = x => x } = (req.sql || {})
+    const { query, postProcessFn = x => x.length === 1 ? x[0] : x } = req.sql
     client = await pool.connect()
     const { rows } = await client.query(query)
     client.release()
     if (!rows.length) return next(new NotFoundError())
-    res.send(rows.length === 1 ? postProcessFn(rows[0]) : rows.map(postProcessFn))
+    res.send(postProcessFn(rows))
   } catch (e) {
     if (client) client.release()
-    next(e)
+    next(new RestError(500, e.message))
   }
 }
 
 const QueryStreamMiddleware = async (req, res, next) => {
   let releaseOnce
   try {
-    const { query, postProcessFn = x => x } = (req.sql || {})
+    const { query, postProcessFn = x => x } = req.sql
     const client = await pool.connect()
     const source = client.query(new QueryStream(query.text, query.values))
     releaseOnce = once(client.release)
@@ -47,7 +47,7 @@ const QueryStreamMiddleware = async (req, res, next) => {
       .pipe(res)
   } catch (e) {
     if (releaseOnce) releaseOnce()
-    next(e)
+    next(new RestError(500, e.message))
   }
 }
 
